c0e3933fd10f310bf913e567b9f7c3925e44ff3a
Gregory Josephs <greg.josephs@gmail.com>
2022-09-29 08:41:54 -0400

    established link

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
         <div>
             Home Page
+            <Link to="create-offering">Create Offering</Link>
         </div>
     );
 }
