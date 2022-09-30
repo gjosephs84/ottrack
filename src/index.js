94eed54e12c67e3435ca63c13f07b858c7169c21
Gregory Josephs <greg.josephs@gmail.com>
2022-09-30 07:21:21 -0400

    got apollo working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 //import the routes
 import Admin from './routes/admin';
@@ -14,22 +14,47 @@ import Home from './routes/home';
 import Login from './routes/login';
 import SelectShifts from './routes/select-shifts';
 
+// Initialize the Apollo Client to be able to work with graphql elsewhere in the app
+
+// NOTE: Will need to change uri for deployment
+
+const client = new ApolloClient({
+  uri: 'http://localhost:1337/graphql',
+  cache: new InMemoryCache()
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+/*
+
+EXPLANATION OF THE FOLLOWING:
+
+-----ApolloClient-----
+    By wrapping everything inside the <React.StrictMode> tags in <ApolloProvider> I am enabling access to Apoolo Client everywhere within. At least, that's how it is supposed to work ...
+
+-----BrowserRouter-----
+    Here I am setting up the routes and calling <Home> as the index element. I can navigate to other pages using the <Link> tag and the paths below referenced.
+
+    The contents of each path express themselves through the <Output> tag in App.js
+
+*/
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />}/>
-          <Route path="login" element={<Login />} />
-          <Route path="admin" element={<Admin />} />
-          <Route path="create-offering" element={<CreateOffering />} />
-          <Route path="employee" element={<Employee />} />
-          <Route path="select-shifts" element={<SelectShifts />} />
-          <Route path="history" element={<History />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />}/>
+            <Route path="login" element={<Login />} />
+            <Route path="admin" element={<Admin />} />
+            <Route path="create-offering" element={<CreateOffering />} />
+            <Route path="employee" element={<Employee />} />
+            <Route path="select-shifts" element={<SelectShifts />} />
+            <Route path="history" element={<History />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
   document.querySelector('#root')
 );
