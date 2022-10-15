9b35beb246fb52d0af1f89dee4f8d8ad9c700a50
Gregory Josephs <greg.josephs@gmail.com>
2022-10-14 20:41:48 -0400

    finished cleaning data for now

--- src/components/offeringRequests.js
+++ src/components/offeringRequests.js
@@ -17,6 +17,7 @@ query GetActiveOfferingsResponses{
                     attributes {
                       users_permissions_user {
                         data {
+                          id
                           attributes {
                             username
                             seniority
@@ -66,10 +67,39 @@ const OfferingsRequests = () => {
     for (let i=0; i<rawOfferings.length; i++) {
         let currentOfferingResponse = rawOfferings[i].attributes.offering_response.data.attributes;
         rawRespondants.push(currentOfferingResponse.respondants);
-        console.log("rawRespondants is: ", rawRespondants);
     };
 
-    // Stopping here for today so I can get the dog out for a walk.
+    // Now let's turn those raw respondants into something actually useable
+
+    const cleanResponses = [];
+    for (let i=0; i<rawRespondants.length; i++) {
+        let currentOfferingResponse = rawRespondants[i].data;
+        for (let j=0; j<currentOfferingResponse.length; j++) {
+            // Let's destructure to save some time and typing
+            const { requested_shifts, users_permissions_user} = currentOfferingResponse[j].attributes;
+            // Now let's destructure some more and make a new clean object array
+            const cleanShifts=[];
+            // We'll need this destructured user data
+            const {username, seniority} = users_permissions_user.data.attributes;
+            const id = users_permissions_user.data.id;
+            requested_shifts.data.forEach(element => {
+                const {shift, ranking} = element.attributes;
+                const cleanShift = {
+                    shift: shift.data.id,
+                    ranking: ranking
+                };
+                cleanShifts.push(cleanShift)
+
+            });
+            cleanResponses.push({
+                username: username,
+                userId: id,
+                seniority: seniority,
+                shiftResponses: cleanShifts
+            });
+        }
+        console.log("cleanResponses is: ", cleanResponses);
+    }
 
 
     return (
