bbf3a7ec29815a393b9ead85c5809ae83d31c308
Gregory Josephs <greg.josephs@gmail.com>
2022-10-18 06:37:25 -0400

    added showrequests toggle button

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,9 +2,11 @@ import React from "react";
 import OfferingsRequests from "../components/offeringRequests";
 
 const Admin = () => {
+    const [showRequests, setShowRequests] = React.useState(false);
     return (
         <div>
-            <OfferingsRequests/>
+            {showRequests && <OfferingsRequests/>}
+            <button onClick={() => {setShowRequests(!showRequests)}}>Show Requests</button>
         </div>
     );
 }
