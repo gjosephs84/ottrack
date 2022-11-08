b1a0a757465f414362761541acb70e7736065540
Gregory Josephs <greg.josephs@gmail.com>
2022-11-07 22:37:53 -0500

    added comments to component

--- src/components/confirmBox.js
+++ src/components/confirmBox.js
@@ -1,3 +1,25 @@
+/*
+
+    ----- CONFIRMBOX COMPONENT -----
+
+    The purpose of this component is to standardize a confirmation alert
+    for certain actions that occur before PUT/POST requests to the API.
+
+    The output is a div against a full-screen blurred background with two
+    button uptions—YES to confirm the action, or NO to clear the alert and
+    allow the user to alter their choices before continuing.
+
+    ConfirmBox takes the following props:
+
+        header: A string to use as the alert header/title
+        message: A string to show what the alert should say
+        handleYes: The callback function to do whatever needs to be confirmed
+        buttonYes: A string representing the text of the yes button
+        setConfirm: The setState of the variable that toggles whether ConfirmBox shows or not
+        buttonNo: A string representing the text of the no button
+
+*/
+
 const ConfirmBox = 
     ({
     header, message, handleYes, buttonYes, setConfirm, buttonNo
